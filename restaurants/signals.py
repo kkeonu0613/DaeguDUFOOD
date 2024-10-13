81619ca49aa660e15b875d21a52066f5df763ed6
kkeonu0613 <129020521+kkeonu0613@users.noreply.github.com>
2024-10-14 08:43:03 +0900

    Delete restaurants directory

--- restaurants/signals.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class RestaurantsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restaurants'  # 애플리케이션의 이름
-
-    def ready(self):
-        import restaurants.signals  # 신호를 임포트
