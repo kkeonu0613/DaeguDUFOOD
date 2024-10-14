4af50aea21ac60702117c081b2692276815ce10c
kkeonu0613 <129020521+kkeonu0613@users.noreply.github.com>
2024-10-14 11:04:42 +0900

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,10 +3,15 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dufood.settings')
+    # 기본적으로 'dufood.settings' 설정 파일을 사용
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dufood.settings')  # 기본 설정
+
+    # 배포 환경일 경우, settings.production을 사용하도록 설정
+    if os.environ.get('DJANGO_ENV') == 'production':
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'dufood.settings.production'  # 배포용 설정
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +20,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
